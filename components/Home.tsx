621fd539ea6c1758c6b623b0edf6039f1ed3b53b
Macias Romero Jorge Humberto <macias_george@outlook.com>
2023-10-21 12:28:17 -0600

    Se hicieron los siguientes cambios:     - Creación de un Action para Continous Integration.     - Modificación de la ruta layout de acuerdo al color en modo obscuro, pasando de un azúl a un negro.     - Creación de animaciones al componente Home para que se vea más fluído el movimiento.     - Reestructuración casi completa del componente Navbar para que se oculte a la hora de hacer scroll hacia abajo, cambio de colores en el modo obscuro para que sea igual al de la ruta layout y algunos detalles estéticos en los links.     - Creación del componente ProjectPopUp para mostrar los detalles y fotos adicionales de cada proyecto.     - Reestructuración casi completa del componente Projects para quitar el scroll cada vez que se abra el componente ProjectPopUp, cambio de colores para que sea igual al de la ruta layout, cambio de tiempos en el cambio automático del carrusel y algunos cambios estéticos.     - Reestructuración casi completa de la constante projects para en un fututo incluír todas las características de un proyecto.     - Agregado de los archivos webp de cada proyecto, conllevando renombrado y reubicación de los mismos.     - Reestructuración de los types para borrar los obsoletos e incluír el de projects.

--- components/Home.tsx
+++ components/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         <div id='Home' className='w-full min-h-screen flex flex-col justify-center items-center text-center p-14 gap-5'>
             <p className='text-4xl md:text-6xl lg:text-8xl animate-fade-down animate-duration-[2000ms]'>{t('title')}</p>
-            <Link href='#About' className='px-6 py-3 text-xl md:text-2xl lg:text-4xl rounded-2xl border-2 border-black hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black animate-fade-down animate-duration-[2000ms] animate-delay-[2000ms]'>{t('button')}</Link>
+            <Link href='#About' className='px-6 py-3 text-xl md:text-2xl lg:text-4xl rounded-2xl border-2 border-black hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black duration-300 ease-in-out animate-fade-down animate-duration-[2000ms] animate-delay-[2000ms]'>{t('button')}</Link>
         </div>
     )
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,28 +3,44 @@
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 import { Carousel, IconButton } from "@material-tailwind/react";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import projects from '@/constants/projects';
-import Link from 'next/link';
+import ProjectPopUp from './ProjectPopUp';
 
 const Projects = () => {
     const t = useTranslations('Projects')
     const [isTextVisible, setIsTextVisible] = useState(false);
+    const [modal, SetModal] = useState(false);
+    const [projectSelected, setProjectSelected] = useState(projects[0]);
+
+    useEffect(() => {
+        if (modal) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [modal]);
 
     return (
         <div id='Projects' className='w-full min-h-screen flex flex-col justify-center items-center text-center p-14 gap-5'>
+            {modal && (
+                <ProjectPopUp lan={t('lan')} project={projectSelected} onClick={() => SetModal(false)} />
+            )}
             <p>{t('title')}</p>
             <Carousel
                 className="rounded-xl"
                 loop
                 autoplay
+                autoplayDelay={7000}
                 navigation={({ setActiveIndex, activeIndex, length }) => (
-                    <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                    <div className="absolute bottom-4 left-2/4 z-30 flex -translate-x-2/4 gap-2 p-1 bg-black/50 rounded-full">
                         {new Array(length).fill("").map((_, i) => (
                             <span
                                 key={i}
-                                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-                                    }`}
+                                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"}`}
                                 onClick={() => setActiveIndex(i)}
                             />
                         ))}
@@ -35,9 +51,9 @@ const Projects = () => {
                         variant="text"
                         color="white"
                         onClick={handlePrev}
-                        className="!absolute top-2/4 left-4 -translate-y-2/4 z-30 bg-black/50 hover:border-2 hover:border-white"
+                        className="!absolute top-2/4 left-4 -translate-y-2/4 z-30 bg-black/50 hover:border-2 hover:border-white hidden sm:block"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-3 h-3 sm:w-5 sm:h-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                         </svg>
                     </IconButton>
@@ -47,9 +63,9 @@ const Projects = () => {
                         variant="text"
                         color="white"
                         onClick={handleNext}
-                        className="!absolute top-2/4 !right-4 -translate-y-2/4 z-30 bg-black/50 hover:border-2 hover:border-white"
+                        className="!absolute top-2/4 !right-4 -translate-y-2/4 z-30 bg-black/50 hover:border-2 hover:border-white hidden sm:block"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-3 h-3 sm:w-5 sm:h-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                         </svg>
                     </IconButton>
@@ -59,10 +75,10 @@ const Projects = () => {
                     <div key={project.id} className='relative w-full h-full flex justify-center items-center' onMouseEnter={() => setIsTextVisible(true)} onMouseLeave={() => setIsTextVisible(false)}>
                         <Image src={project.img} alt="Project ilustration" width={1880} height={800} className={`${!isTextVisible && 'animate-pulse animate-ease-in animate-duration-[5000ms]'}`} priority />
                         {isTextVisible && (
-                            <div className='w-full h-full overflow-y-scroll overflow-x-hidden py-5 px-20 lg:p-10 absolute top-0 flex flex-col justify-start lg:justify-end items-center bg-black/50 animate-fade' onClick={() => setIsTextVisible(false)}>
+                            <div className='w-full h-full py-5 px-20 sm:p-10 absolute flex flex-col justify-center sm:justify-end items-center bg-black/50 animate-fade' onClick={() => setIsTextVisible(false)}>
                                 <p className='z-20 text-xl md:text-2xl lg:text-4xl text-white'>{project.title}</p>
-                                <p className='z-20 text-base md:text-lg lg:text-2xl text-gray-300'>{t('lan') === 'en' ? project.description : project.description_es}</p>
-                                <Link href="/" className='z-20 rounded-xl px-4 py-1 text-white border border-black bg-black hover:border-white'>More</Link>
+                                <p className='z-20 text-base md:text-lg lg:text-2xl text-gray-300 hidden sm:block'>{t('lan') === 'en' ? project.description : project.description_es}</p>
+                                <button onClick={() => { SetModal(true); setProjectSelected(project) }} className='z-20 rounded-xl px-4 py-1 text-white border border-black bg-black hover:border-white duration-300 ease-in-out'>{t('lan') === 'en' ? 'More' : 'Más'}</button>
                             </div>
                         )}
                     </div>
